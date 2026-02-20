ba92ba1199f99de2ba9e5accfb069663e6517108
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2026-02-20 19:56:18 +0530

    code added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build",
-  },
   server: {
     historyApiFallback: true, // Ensures React Router works correctly
   },
